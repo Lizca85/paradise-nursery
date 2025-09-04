e162aa13fb1d13544f3ba9a08220737bdb2d98ea
Lizca85 <zago85@hotmail.com>
2025-09-04 14:28:29 -0500

    actualizacion

--- src/data/plants.js
+++ src/data/plants.js
@@ -3,7 +3,7 @@ export const plantsArray = [
     id: 'mint',
     name: 'Menta',
     category: 'Aromáticas',
-    image: 'https://images.unsplash.com/photo-1598899134739-24c46f58b8c0?q=80&w=1200&auto=format&fit=crop',
+    image: 'https://media.istockphoto.com/id/1164307019/es/foto/ingrediente-fresco-de-las-hierbas-verdes-de-menta.jpg?s=1024x1024&w=is&k=20&c=t-3lpVmd4CKZu7ECMcIlTgWvh5ghotHIVNSxtZQD1PM=',
     description: 'Refrescante y perfecta para infusiones y postres. Fácil de cuidar.',
     cost: '$12.00'
   },
@@ -11,7 +11,7 @@ export const plantsArray = [
     id: 'basil',
     name: 'Albahaca',
     category: 'Aromáticas',
-    image: 'https://images.unsplash.com/photo-1599382102945-3e8f302ff628?q=80&w=1200&auto=format&fit=crop',
+    image: 'https://images.pexels.com/photos/1087902/pexels-photo-1087902.jpeg',
     description: 'Aromática estrella de la cocina mediterránea.',
     cost: '$15.00'
   },
@@ -19,7 +19,7 @@ export const plantsArray = [
     id: 'rosemary',
     name: 'Romero',
     category: 'Aromáticas',
-    image: 'https://images.unsplash.com/photo-1615485737641-3dc1ff670a4a?q=80&w=1200&auto=format&fit=crop',
+    image: 'https://media.istockphoto.com/id/598931180/es/foto/albahaca-salvia-eneldo-y-hierbas-de-tomillo.jpg?s=1024x1024&w=is&k=20&c=DndQdJTiBKfoLwa22QZiY7weuFlvTNxJ3j7yxRlTCiw=',
     description: 'Notas resinosas para carnes y panes. Muy resistente.',
     cost: '$13.00'
   },
@@ -27,7 +27,7 @@ export const plantsArray = [
     id: 'aloe',
     name: 'Sábila',
     category: 'Medicinales',
-    image: 'https://images.unsplash.com/photo-1604977040277-2d9d3f1b3df0?q=80&w=1200&auto=format&fit=crop',
+    image: 'https://media.istockphoto.com/id/1345935119/es/foto/aloe-o-aloe-vera-hojas-frescas-y-rodajas-sobre-fondo-blanco.jpg?s=1024x1024&w=is&k=20&c=XCOawLDYdeqgF2LVipOjLLrMSCjdg8YcobPpPnH4P9Y=',
     description: 'Gel calmante para la piel. Requiere poca agua.',
     cost: '$18.00'
   },
@@ -35,7 +35,7 @@ export const plantsArray = [
     id: 'chamomile',
     name: 'Manzanilla',
     category: 'Medicinales',
-    image: 'https://images.unsplash.com/photo-1568658949245-9e5f5e9057d1?q=80&w=1200&auto=format&fit=crop',
+    image: 'https://media.istockphoto.com/id/1277086333/es/foto/t%C3%A9-de-manzanilla-de-hierbas-y-flores-de-manzanilla-cerca-de-tetera-y-cristal-de-t%C3%A9-sobre-mesa.jpg?s=1024x1024&w=is&k=20&c=LhqITYIT2ugTQK4tpWtcPHX7st61cuctmLmQltmY6cU=',
     description: 'Infusiones suaves para relajarte antes de dormir.',
     cost: '$11.00'
   },
@@ -43,7 +43,7 @@ export const plantsArray = [
     id: 'lavender',
     name: 'Lavanda',
     category: 'Medicinales',
-    image: 'https://images.unsplash.com/photo-1466781783364-36c955e42a7f?q=80&w=1200&auto=format&fit=crop',
+    image: 'https://media.istockphoto.com/id/1409278584/es/foto/paquete-de-flores-de-lavanda-frescas-sobre-un-blanco.jpg?s=1024x1024&w=is&k=20&c=Rp5JI4zdevCbuNL23iQP6un0ojz1Ua8Illeb6_k4UP0=',
     description: 'Aroma relajante. Atrae polinizadores.',
     cost: '$16.00'
   }
