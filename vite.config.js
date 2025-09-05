1da1d24a07b86cc858e2e6282095bdc96fc995ab
Lizca85 <101907191+Lizca85@users.noreply.github.com>
2025-09-05 08:44:03 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Cambia 'paradise-nursery' por el nombre de tu repo si usas GitHub Pages
   base: '/paradise-nursery/',
 })
